9f4b340e854a64e3c7ce99186015a14160b126ae
WerakulSrihahsan <43484439+WerakulSrihahsan@users.noreply.github.com>
2019-03-11 21:41:13 -0400

    Update Save.js

--- Save.js
+++ Save.js
@@ -1,18 +1,17 @@
 function doFirst(){
 	var button = document.getElementById("button");
-	button.addEventListener("click", Saves, false) ;
+	button.addEventListener("click", Saves , false) ;
 }
 function Saves(){
 	var one = document.getElementById("one").value;
-	var one = document.getElementById("two").value;
-	var one = document.getElementById("three").value;
-	var one = document.getElementById("four").value;
+	var two = document.getElementById("two").value;
 	sessionStorage.setItem(one,two);
+	
 	display(one);
 }
 function display(one){
-	var rightboc = document.getElementById("rightboc");
+	var rightbox = document.getElementById("rightbox");
 	var two = sessionStorage.getItem(one) ; 
-	rightboc.innerHTML = "Date: " + one +"<br />Team:"+Two;
+	rightbox.innerHTML = "Date: " + one +"<br />Team:"+Two;
 }
 widow.addEventListener("load", doFirst, false);
